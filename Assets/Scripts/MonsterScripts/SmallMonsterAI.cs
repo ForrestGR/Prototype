85e29372910df20a3f439d625e7e9e6e21ee7997
ForrestGR <f.brede@hotmail.de>
2024-06-05 03:12:07 +0200

    update

--- Assets/Scripts/MonsterScripts/SmallMonsterAI.cs
+++ Assets/Scripts/MonsterScripts/SmallMonsterAI.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SmallMonsterAI : BaseMonsterAI
+public class SmallMonsterAI : MonoBehaviour
 {
     // Extra Logik für die KI des kleinen Monsters
 
