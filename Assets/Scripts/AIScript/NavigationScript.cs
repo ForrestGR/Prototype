65e2e2a72484552a28f08179e45afa30d970a7d1
ForrestGR <f.brede@hotmail.de>
2024-06-19 03:32:18 +0200

    NAVMESH

---
+++ Assets/Scripts/AIScript/NavigationScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavigationScript : MonoBehaviour
+{
+    public Transform player;
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+
+    private void Update()
+    {
+        agent.destination = player.position;
+    }
+
+}
+
