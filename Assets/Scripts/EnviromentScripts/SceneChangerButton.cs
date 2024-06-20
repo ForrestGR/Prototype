a6022220bf0add693698753513ebadd03d0603bc
ForrestGR <f.brede@hotmail.de>
2024-06-20 20:06:32 +0200

    dontdestroyonload

---
+++ Assets/Scripts/EnviromentScripts/SceneChangerButton.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangerButton : MonoBehaviour
+{
+    public string sceneName = "FPS";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Überprüfe, ob der kollidierende Gegenstand der Spieler ist
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    private static MenuManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
